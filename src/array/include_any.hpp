fc4c3ed918a6a12a18eb0077f00f962dc4c9d66f
hun756 <ekemenms@hotmail.com>
2021-08-07 16:23:58 +0300

    Add Include Any

---
+++ src/array/include_any.hpp
@@ -0,0 +1,24 @@
+#ifndef ARRAY_INCLUDE_ANY_HPP
+#define ARRAY_INCLUDE_ANY_HPP
+
+#include <vector>
+#include <algorithm>
+
+namespace Array
+{
+    template <typename T>
+    static bool includesAny(std::vector<T> &nums, std::vector<T> &incs)
+    {
+        for (auto &&_incVars : incs)
+        {
+            if (std::find(nums.begin(), nums.end(), _incVars) != nums.end())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+} // namespace Array
+
+#endif /* end of include guard :  ARRAY_INCLUDE_ANY_HPP */
