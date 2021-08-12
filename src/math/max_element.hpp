a2429159da8d1cfb3544a878c1660b28f150740c
hun756 <ekemenms@hotmail.com>
2021-08-12 17:09:15 +0300

    Add Max Element

---
+++ src/math/max_element.hpp
@@ -0,0 +1,39 @@
+#ifndef MATH_MAX_ELEMENT_HPP
+#define MATH_MAX_ELEMENT_HPP
+
+#include <cassert>
+
+namespace Math
+{
+    template<class FwIterator, class Compare>
+    auto maxElement(FwIterator _begin, FwIterator _end, Compare cmp)
+    {
+        assert(_begin != _end);
+
+        FwIterator maxElem = _begin++;
+
+        while (_begin != _end)
+        {
+            if (cmp(*maxElem, *_begin))
+                maxElem = _begin;
+            ++_begin;
+        }
+        return *maxElem;
+    }    
+
+    template<class FwIterator>
+    auto maxElement(FwIterator _begin, FwIterator _end) 
+    {
+        return maxElement(
+                _begin, 
+                _end, 
+                [](auto a, auto b) { 
+                    return a  < b;
+                }
+        );
+    }
+
+} // namespace Math
+
+
+#endif /* end of include guard :  MATH_MAX_ELEMENT_HPP */
