a0661301157ee540e84aedd93f5671f3ee272c94
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2024-03-22 22:56:08 +0300

    Refactor: Update Distance Calc with Templates & Classes and Add Tests
     - Implemented template-based distance calculation and added extensive tests.
     - Supports `Point`, `std::pair`, `std::tuple`

--- src/algorithm/distance.hpp
+++ src/algorithm/distance.hpp
@@ -1,15 +1,46 @@
-#ifndef ALFORITHM_DISTANCE_HPP
-#define ALFORITHM_DISTANCE_HPP
+#ifndef ALFORITHM_DISTANCE_HPP_cQUhGm
+#define ALFORITHM_DISTANCE_HPP_cQUhGm
 
 #include <cmath>
+#include <utility>
+#include <tuple>
 
 namespace Algorithm 
 {
-    static double distance(double x0, double y0, double x1, double y1)
+    template<typename T>
+    class Point 
     {
-        return std::hypot(x1 - x0, y1 - y0);    
+    public:
+        T x, y;
+
+        Point(T x, T y) : x(x), y(y) {}
+
+        template<typename U>
+        double distanceTo(const Point<U>& other) const 
+        {
+            return std::hypot(other.x - x, other.y - y);
+        }
+    };
+
+    template<typename T, typename U>
+    double distance(const Point<T>& p1, const Point<U>& p2) 
+    {
+        return p1.distanceTo(p2);
     }
+
+    template<typename T, typename U>
+    double distance(const std::pair<T, T>& p1, const std::pair<U, U>& p2) 
+    {
+        return std::hypot(p2.first - p1.first, p2.second - p1.second);
+    }
+
+    template<typename T, typename U>
+    double distance(const std::tuple<T, T>& p1, const std::tuple<U, U>& p2) 
+    {
+        return std::hypot(std::get<0>(p2) - std::get<0>(p1), std::get<1>(p2) - std::get<1>(p1));
+    }
+
 } // namespace Algorıthm
 
 
-#endif /* end of include guard :  ALFORITHM_DISTANCE_HPP */
+#endif /* end of include guard :  ALFORITHM_DISTANCE_HPP_cQUhGm */
--- test/distance.cpp
+++ test/distance.cpp
@@ -2,45 +2,54 @@
 #include "../external/catch2/catch.hpp"
 #include "../src/algorithm/distance.hpp"
 
-#include <vector>
 #include <string>
-#include <limits>
-#include <cmath>
+
+// static bool isSame(double x, double y)
+// {
+//     return std::fabs(x - y) < std::numeric_limits<double>::epsilon() ;
+// }
 
 
-static bool isSame(double x, double y)
-{
-    return std::fabs(x - y) < std::numeric_limits<double>::epsilon() ;
-}
+TEST_CASE("Distance Calculation Tests", "[distance]") {
+    using namespace Algorithm;
 
+    SECTION("Point to Point Distance") {
+        Point<double> p1(0.0, 0.0);
+        Point<double> p2(3.0, 4.0);
 
-TEST_CASE("Distance Test", "[distance]")
-{
-    struct TestStruct
-    {
-        double pointX0;
-        double pointX1;
-        double pointY0;
-        double pointY1;
+        REQUIRE(p1.distanceTo(p2) == Approx(5.0).epsilon(0.001));
+    }
 
-        double actual;
+    SECTION("Different Types") {
+        Point<int> p1(0, 0);
+        Point<float> p2(3.0f, 4.0f);
 
-        TestStruct(double pointX0, double pointX1, 
-            double pointY0,double pointY1, 
-            double actual
-        )   :   pointX0(pointX0), pointX1(pointX1), 
-                pointY0(pointY0), pointY1(pointY1),
-            actual(actual) 
-        {
-        }
-    };
+        REQUIRE(p1.distanceTo(p2) == Approx(5.0).epsilon(0.001));
+    }
 
-    std::vector<TestStruct> testVec;
+    SECTION("Negative Coordinates") {
+        Point<double> p1(-1.0, -1.0);
+        Point<double> p2(-4.0, -5.0);
 
-    testVec.emplace_back(1.0, 1.0, 4.0, 5.0, 5.0);
+        REQUIRE(p1.distanceTo(p2) == Approx(5.0).epsilon(0.001));
+    }
 
-    for (auto &&var : testVec)
-    {
-        REQUIRE(isSame(Algorithm::distance(var.pointX0, var.pointX1, var.pointY0, var.pointY1), var.actual));
+    SECTION("Extreme Values") {
+        Point<long long> p1(LLONG_MIN, LLONG_MIN);
+        Point<long long> p2(LLONG_MAX, LLONG_MAX);
+
+        REQUIRE_NOTHROW(p1.distanceTo(p2));
+    }
+
+    SECTION("Using std::pair and std::tuple") {
+        std::pair<int, int> p1 = {0, 0};
+        std::pair<double, double> p2 = {3.0, 4.0};
+
+        REQUIRE(Algorithm::distance(p1, p2) == Approx(5.0).epsilon(0.001));
+
+        std::tuple<int, int> t1 = std::make_tuple(0, 0);
+        std::tuple<double, double> t2 = std::make_tuple(3.0, 4.0);
+
+        REQUIRE(Algorithm::distance(t1, t2) == Approx(5.0).epsilon(0.001));
     }
 }
