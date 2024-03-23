72e0f805d0c65176d9d6cdd43363937fb8320811
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2024-03-23 05:57:14 +0300

    Added a specialized function for counting value frequencies

--- src/algorithm/frequency.hpp
+++ src/algorithm/frequency.hpp
@@ -1,20 +1,40 @@
-#ifndef ALGORITHM_FREQUENCY_HPP
-#define ALGORITHM_FREQUENCY_HPP
+#ifndef ALGORITHM_FREQUENCY_HPP_lPJDBv
+#define ALGORITHM_FREQUENCY_HPP_lPJDBv
 
 #include <unordered_map>
 
-namespace Algorithm
+namespace Algorithm 
 {
-    template <typename T, typename U>
-    std::unordered_map<T, size_t> frequency(U val)
-    {
-        std::unordered_map<T, size_t> mp;
-        for (auto&& var : val)
-            mp[var]++;
+    template <typename T> struct is_pair : std::false_type {};
 
-        return mp;
+    template <typename T, typename U>
+    struct is_pair<std::pair<T, U>> : std::true_type {};
+
+    template <typename T, typename Container>
+    auto frequency(const Container &values) ->
+        typename std::enable_if<is_pair<typename Container::value_type>::value,
+                                std::unordered_map<T, size_t>>::type 
+    {
+        std::unordered_map<T, size_t> frequencies;
+        for (const auto &element : values)
+        {
+            ++frequencies[element.first];
+        }
+        return frequencies;
+    }
+
+    template <typename T, typename Container>
+    auto frequency(const Container &values) ->
+        typename std::enable_if<!is_pair<typename Container::value_type>::value,
+                                std::unordered_map<T, size_t>>::type 
+    {
+        std::unordered_map<T, size_t> frequencies;
+        for (const auto &element : values)
+        {
+            ++frequencies[element];
+        }
+        return frequencies;
     }
 } // namespace Algorithm
 
-
-#endif /* end of include guard :  ALGORITHM_FREQUENCY_HPP */
+#endif /* end of include guard :  ALGORITHM_FREQUENCY_HPP_lPJDBv */
--- test/frequency.cpp
+++ test/frequency.cpp
@@ -1,64 +1,95 @@
 #define CATCH_CONFIG_MAIN
-#include "../external/catch2/catch.hpp"
 #include "../src/algorithm/frequency.hpp"
+#include "../external/catch2/catch.hpp"
 
-#include <vector>
+#include <functional>
 #include <string>
+#include <vector>
 
-template <typename T, typename U>
-struct TestStruct
+class NonDefaultConstructible
 {
-    U val;
+private:
+    int value;
 
-    std::unordered_map<T, size_t> actual;
+public:
+    explicit NonDefaultConstructible(int v) : value(v) {}
 
-    TestStruct(U val, std::unordered_map<T, size_t> actual)
-        : val(val), actual(actual)
-    {
-    }
+    bool operator==(const NonDefaultConstructible& other) const { return value == other.value; }
+
+    bool operator<(const NonDefaultConstructible& other) const { return value < other.value; }
+
+    int getValue() const { return value; }
 };
 
-TEST_CASE("Frequency Test", "[frequency]")
+namespace std
 {
-    std::vector<TestStruct<char, std::string>> testVec{
-        {"hello",
-         {std::pair<char, size_t>('h', 1),
-          std::pair<char, size_t>('e', 1),
-          std::pair<char, size_t>('l', 2),
-          std::pair<char, size_t>('o', 1)}}};
-
-    for (auto &&var : testVec)
-    {
-        REQUIRE(Algorithm::frequency<char, std::string>(var.val) == var.actual);
-    }
-}
-
-
-/*
-TEST_CASE("Frequency Test With Vector Container", "[frequency]")
-{
-    std::vector<TestStruct<std::string, std::vector<std::string>>> testVec{
+    template <>
+    struct hash<NonDefaultConstructible> {
+        std::size_t operator()(const NonDefaultConstructible& ndc) const noexcept
         {
-            {
-                "melon",
-                "watermelon",
-                "cheery"
-                "melon"
-            },
-            {
-                std::pair<std::string, size_t>("melon", 2),
-                std::pair<std::string, size_t>("watermelon", 1),
-                std::pair<std::string, size_t>("cheery", 1)
-            }
+            return std::hash<int>()(ndc.getValue());
         }
     };
+} // namespace std
 
-    for (auto &&var : testVec)
-    {
-        REQUIRE(Algorithm::frequency<
-                    std::string, 
-                    std::vector<std::string>
-                    >(var.val) == var.actual);
-    }
+TEST_CASE("Frequency counts are correct for vector of ints", "[frequency]")
+{
+    std::vector<int> vec = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+    auto freq = Algorithm::frequency<int>(vec);
+
+    REQUIRE(freq[1] == 1);
+    REQUIRE(freq[2] == 2);
+    REQUIRE(freq[3] == 3);
+    REQUIRE(freq[4] == 4);
 }
-*/
+
+TEST_CASE("Frequency counts are correct for list of chars", "[frequency]")
+{
+    std::list<char> lst = {'a', 'b', 'b', 'c', 'c', 'c'};
+    auto freq = Algorithm::frequency<char>(lst);
+
+    REQUIRE(freq['a'] == 1);
+    REQUIRE(freq['b'] == 2);
+    REQUIRE(freq['c'] == 3);
+}
+
+TEST_CASE("Frequency function works with empty container", "[frequency]")
+{
+    std::vector<int> emptyVec;
+    auto freq = Algorithm::frequency<int>(emptyVec);
+
+    REQUIRE(freq.empty());
+}
+
+TEST_CASE("Frequency counts with strings", "[frequency]")
+{
+    std::vector<std::string> strings = {"apple", "banana", "apple", "orange", "banana", "banana"};
+    auto freq = Algorithm::frequency<std::string>(strings);
+
+    REQUIRE(freq["apple"] == 2);
+    REQUIRE(freq["banana"] == 3);
+    REQUIRE(freq["orange"] == 1);
+}
+
+TEST_CASE("Frequency works with non-default constructible types", "[frequency]")
+{
+    std::vector<NonDefaultConstructible> ndcVec = {NonDefaultConstructible(1), NonDefaultConstructible(2),
+                                                   NonDefaultConstructible(2), NonDefaultConstructible(3),
+                                                   NonDefaultConstructible(3), NonDefaultConstructible(3)};
+
+    auto freq = Algorithm::frequency<NonDefaultConstructible>(ndcVec);
+
+    REQUIRE(freq[NonDefaultConstructible(1)] == 1);
+    REQUIRE(freq[NonDefaultConstructible(2)] == 2);
+    REQUIRE(freq[NonDefaultConstructible(3)] == 3);
+}
+
+TEST_CASE("Frequency counts for multimap container elements", "[frequency]")
+{
+    std::multimap<std::string, int> multimap = {{"apple", 1}, {"banana", 2}, {"cherry", 3}, {"banana", 4}};
+    auto freq = Algorithm::frequency<std::string>(multimap);
+
+    REQUIRE(freq["apple"] == 1);
+    REQUIRE(freq["banana"] == 2);
+    REQUIRE(freq["cherry"] == 1);
+}
