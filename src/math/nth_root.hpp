5726fa67231781823dceb8fdd829bd90fa45c30b
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2024-03-23 10:50:18 +0300

    Implement NthRootCalculator and comprehensive tests
    Implemented NthRootCalculator with Newton-Raphson and error checks.
    
    Included comprehensive tests using Catch2, covering:
    - Square, cube, and nth root calculations.
    - Negative base handling for odd roots.
    - Exception verification for invalid inputs.

--- src/math/nth_root.hpp
+++ src/math/nth_root.hpp
@@ -1,15 +1,44 @@
-#ifndef NTH_ROOT_HPP
-#define NTH_ROOT_HPP
+#ifndef NTH_ROOT_HPP_CzklKY
+#define NTH_ROOT_HPP_CzklKY
 
 #include <cmath>
+#include <stdexcept>
 
 namespace Math
 {
-    static long int nthRoot(double x, double y)
+    template <typename T, typename = std::enable_if_t<std::is_floating_point<T>::value>>
+    class NthRootCalculator
     {
-        return pow(x, 1.0 / y);
-    }
-} // namespace MAth
+    public:
+        static T calculate(T base, T n, T tolerance = static_cast<T>(0.0001))
+        {
+            validateInput(base, n);
+            T initialGuess = base / n;
+            T root = refineEstimate(base, n, initialGuess, tolerance);
+            return root;
+        }
 
+    private:
+        static void validateInput(T base, T n)
+        {
+            if (n <= 0) {
+                throw std::invalid_argument("n must be greater than 0.");
+            }
+            if (base < 0 && static_cast<int>(n) % 2 == 0) {
+                throw std::invalid_argument("Cannot calculate the even root of a negative number.");
+            }
+        }
 
-#endif /* end of include guard :  NTH_ROOT_HPP */
+        static T refineEstimate(T base, T n, T guess, T tolerance)
+        {
+            T newEstimate = (1 / n) * ((n - 1) * guess + base / std::pow(guess, n - 1));
+            while (std::abs(newEstimate - guess) > tolerance) {
+                guess = newEstimate;
+                newEstimate = (1 / n) * ((n - 1) * guess + base / std::pow(guess, n - 1));
+            }
+            return newEstimate;
+        }
+    };
+} // namespace Math
+
+#endif /* end of include guard :  NTH_ROOT_HPP_CzklKY */
